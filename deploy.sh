389fc3b3cf6e89c41b62c0af0e294dc62978e431
潘静怡 <1003346758@qq.com>
2022-09-20 17:28:51 +0800

    yml

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,7 @@
-echo -e "---------docker Login--------"
-docker login --username=$1 registry.cn-shanghai.aliyuncs.com --password=$2
-
-echo -e "---------停止旧版容器--------"
-docker stop actions 
-
-echo -e "--------- # 删除旧版容器--------"
+docker pull actions-images
+docker stop actions
 docker rm actions
+docker run --name actions -d -p 5555:80 actions-images
+docker image prune -f
+docker container prune -f
 
-echo -e "-----------# 删除旧版镜像----------------"
-docker rmi registry.cn-shanghai.aliyuncs.com/pjy_github/actions:latest
-
-echo -e "---------docker Pull--------"
-docker pull registry.cn-shanghai.aliyuncs.com/pjy_github/actions:latest
-
-echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 5555:80 --name actions registry.cn-shanghai.aliyuncs.com/pjy_github/actions:latest
-
-echo -e "---------deploy Success--------"
