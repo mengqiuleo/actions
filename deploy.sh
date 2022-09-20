2ca1bec267929e5e894ef042a7d9153a3bd93523
潘静怡 <1003346758@qq.com>
2022-09-20 16:51:35 +0800

    yml

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,18 @@ echo -e "---------docker Login--------"
 docker login --username=$1 registry.cn-shanghai.aliyuncs.com --password=$2
 
 echo -e "---------停止旧版容器--------"
-docker stop $(docker ps --filter ancestor=${{ secrets.DOCKER_REPOSITORY }} -q)
+docker stop ${{ secrets.DOCKER_REPOSITORY }} 
 
 echo -e "--------- # 删除旧版容器--------"
-docker rm -f $(docker ps -a --filter ancestor=${{ secrets.DOCKER_REPOSITORY }}:latest -q)
+docker rm ${{ secrets.DOCKER_REPOSITORY }}
 
 echo -e "-----------# 删除旧版镜像----------------"
-docker rmi -f $(docker images ${{ secrets.DOCKER_REPOSITORY }}:latest -q)
+docker rmi registry.cn-shanghai.aliyuncs.com/pjy_github/${{ secrets.DOCKER_REPOSITORY }}:latest
 
 echo -e "---------docker Pull--------"
-docker pull ${{ secrets.DOCKER_REPOSITORY }}:latest
+docker pull registry.cn-shanghai.aliyuncs.com/pjy_github/${{ secrets.DOCKER_REPOSITORY }}:latest
 
 echo -e "---------docker Create and Start--------"
-docker run -d -p 5555:80 ${{ secrets.DOCKER_REPOSITORY }}:latest
+docker run --rm -d -p 5555:80 --name ${{ secrets.DOCKER_REPOSITORY }} registry.cn-shanghai.aliyuncs.com/pjy_github/${{ secrets.DOCKER_REPOSITORY }}:latest
 
 echo -e "---------deploy Success--------"
