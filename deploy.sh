5bb8fe323cfda88b42a7ac3edd33c9ec06bad166
潘静怡 <1003346758@qq.com>
2022-09-20 17:57:24 +0800

    cicd

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-docker pull actions-images
+docker pull actions-images:latest
 docker stop actions
 docker rm actions
-docker run --name actions -d -p 5555:80 actions-images
+docker run --name actions -d -p 5555:80 actions-images:latest
 docker image prune -f
 docker container prune -f
 
