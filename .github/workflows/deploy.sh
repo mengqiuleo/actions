1e64afe9814c168bf5e9f5f2e2a02b323a628879
潘静怡 <1003346758@qq.com>
2022-09-20 21:59:41 +0800

    cicd

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,14 +1,14 @@
-echo -e "---------docker Pull--------"
+
 docker pull mengqiuleo/actions-image:latest
 
-echo -e "---------docker Stop--------"
+
 docker stop actions
 
-echo -e "---------docker Rm--------"
+
 docker rm actions
 docker rmi mengqiuleo/actions-image:latest
 
-echo -e "---------docker Create and Start--------"
+
 docker run --name actions -d -p 5555:80 mengqiuleo/actions-image:latest
 
 
