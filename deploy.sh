4b21ddbc980c5d33d127fb193cec1a87c4ac5f31
潘静怡 <1003346758@qq.com>
2022-09-20 16:57:18 +0800

    yml

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,18 @@ echo -e "---------docker Login--------"
 docker login --username=$1 registry.cn-shanghai.aliyuncs.com --password=$2
 
 echo -e "---------停止旧版容器--------"
-docker stop ${{ secrets.DOCKER_REPOSITORY }} 
+docker stop actions 
 
 echo -e "--------- # 删除旧版容器--------"
-docker rm ${{ secrets.DOCKER_REPOSITORY }}
+docker rm actions
 
 echo -e "-----------# 删除旧版镜像----------------"
-docker rmi registry.cn-shanghai.aliyuncs.com/pjy_github/${{ secrets.DOCKER_REPOSITORY }}:latest
+docker rmi registry.cn-shanghai.aliyuncs.com/pjy_github/actions:latest
 
 echo -e "---------docker Pull--------"
-docker pull registry.cn-shanghai.aliyuncs.com/pjy_github/${{ secrets.DOCKER_REPOSITORY }}:latest
+docker pull registry.cn-shanghai.aliyuncs.com/pjy_github/actions:latest
 
 echo -e "---------docker Create and Start--------"
-docker run --rm -d -p 5555:80 --name ${{ secrets.DOCKER_REPOSITORY }} registry.cn-shanghai.aliyuncs.com/pjy_github/${{ secrets.DOCKER_REPOSITORY }}:latest
+docker run --rm -d -p 5555:80 --name actions registry.cn-shanghai.aliyuncs.com/pjy_github/actions:latest
 
 echo -e "---------deploy Success--------"
